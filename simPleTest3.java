89b25fe9d1a5bb7d1a7875650d0ee75c645e98f0
AD01436156\User <nvqa_a01@naver.com>
2019-05-23 16:42:23 +0900

    modify

--- simPleTest3.java
+++ simPleTest3.java
@@ -4,8 +4,8 @@ import com.google.gson.JsonObject;
 
 public class AppTest {
     public static JsonObject main(JsonObject args) {
-        String name = "World";
-        String place = "Naver";
+        String name = "World!!";
+        String place = "Naver!!!";
         if (args.has("name"))
             name = args.getAsJsonPrimitive("name").getAsString();
         if (args.has("place")) {
